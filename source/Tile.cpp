3133ca39ab8a8d8d1d07602663a4f122564741a3
kudr.max <mdkudryashov@edu.hse.ru>
2024-01-05 13:03:25 +0300

    Fixed Tile rotation in constructor

--- source/Tile.cpp
+++ source/Tile.cpp
@@ -2,8 +2,8 @@
 
 Tile::Tile(const std::string&& texture_path,
            const std::vector<size_t>&& eds,
-           size_t rotation = 0) : texture_str(texture_path),
+           size_t rotation) : texture_str(texture_path),
                                   rotation(rotation) {
     for (int i = 0; i < 4; i++)
-        edges.push_back(eds[i + rotation % 4]);
+        edges.push_back(eds[(i + rotation) % 4]);
 }
--- source/Tile.h
+++ source/Tile.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 class Tile {
@@ -9,9 +10,16 @@ public:
     Tile(const std::string&& texture_path,
          const std::vector<size_t>&& eds,
          size_t rotation);
+    void print() {
+        std::cout << "{ ";
+        for (auto edge : edges) {
+            std::cout << edge << " ";
+        }
+        std::cout << "}\n";
+    }
 public:
     std::vector<size_t> edges;
     std::string texture_str;
     sf::Sprite sprite;
-    size_t rotation;
+    size_t rotation = 0;
 };
